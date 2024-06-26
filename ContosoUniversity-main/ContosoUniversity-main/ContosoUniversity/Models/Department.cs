f360cdd06ff41aff276aa47727bd633c348290eb
Juan Carranza <juansebastiancarranza4@gmail.com>
2024-06-25 23:24:49 -0500

    Se agrega el proyecto

---
+++ ContosoUniversity-main/ContosoUniversity-main/ContosoUniversity/Models/Department.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models;
+
+public class Department
+{
+     
+    public int DepartmentID { get; set; }
+
+    [StringLength(50, MinimumLength = 3)]
+
+
+    [Display(Name = "Nombre")] 
+    public string Name { get; set; }
+    [Display(Name = "Presupuesto")]
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "money")]
+    public decimal Budget { get; set; }
+    
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",
+                   ApplyFormatInEditMode = true)]
+    [Display(Name = "Fecha de creación")]
+    public DateTime StartDate { get; set; }
+
+    public int? InstructorID { get; set; }
+
+    [Timestamp]
+    [Display(Name = "Token")]
+    public byte[] ConcurrencyToken { get; set; }
+
+    public Instructor Administrator { get; set; }
+    public ICollection<Course> Courses { get; set; }
+}
---
+++ ContosoUniversity-main/ContosoUniversity-main/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models;
+
+public enum Grade
+{
+    A, B, C, D, F
+}
+
+public class Enrollment
+{
+
+    public int EnrollmentID { get; set; }
+    [Display(Name = "Titulo")]
+    public int CourseID { get; set; }
+    public int StudentID { get; set; }
+    [DisplayFormat(NullDisplayText = "No grade")]
+    [Display(Name = "Grado")]
+    public Grade? Grade { get; set; }
+
+    public Course Course { get; set; }
+    public Student Student { get; set; }
+}
